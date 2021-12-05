73a0fef18ed9573f96e7b50b5695b845ba0f6ea7
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2021-12-05 19:11:31 +0100

    Changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,30 @@
-$(window).on('load',function(){
-    $('#popup').modal('show');
+/* Popup Window */
+
+$(function(){
+    $("#popupwindow").load("/elements/popup.html"); 
 });
+
+  $(document).mouseup(function(e) {
+    var container = $("#popupwindow");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+
+/* Sidemenu */
+
+let menuTitle = document.getElementById("sidemenu-title");
+
+function menuToggle() {
+
+    let menuContent = document.getElementById("sidemenu-content");
+    if (menuContent.style.display == "none") {
+        menuContent.style.display = "block";
+      } else {
+        menuContent.style.display = "none";
+      }
+}
+
+document.addEventListener("click", menuToggle);
