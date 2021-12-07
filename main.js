8104a8b8ff8f0565b03c8543a4c741f434ab00ee
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2021-12-07 10:34:56 +0100

    Things

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 /* Popup Window */
 
-$(function(){
-    $("#popupwindow").load("popup.html"); 
+$(function () {
+  $("#popupwindow").load("popup.html");
 });
 
-$(document).mouseup(function(e) {
-    let container = $("#popupwindow");
+$(document).mouseup(function (e) {
+  let container = $("#popupwindow");
 
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-        container.hide();
-    }
+  // if the target of the click isn't the container nor a descendant of the container
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    container.hide();
+  }
 });
 
 /* Sidemenu */
 function menuToggle() {
-    let menuContent = document.getElementById("sidemenu-content");
-    if (menuContent.style.display == "block") {
-        menuContent.style.display = "none";
-        document.getElementById("sidemenu-title").innerHTML = "<h1>⮙ Explore the tales</h1>";
-
-      } else {
-        menuContent.style.display = "block";
-        document.getElementById("sidemenu-title").innerHTML = "<h1>⮛ Explore the tales</h1>";
-      }      
+  let menuContent = document.getElementById("sidemenu-content");
+  if (menuContent.style.display == "block") {
+    menuContent.style.display = "none";
+    document.getElementById("sidemenu-title").innerHTML =
+      "<h1>⮙ Explore the tales</h1>";
+  } else {
+    menuContent.style.display = "block";
+    document.getElementById("sidemenu-title").innerHTML =
+      "<h1>⮛ Explore the tales</h1>";
+  }
 }
-
