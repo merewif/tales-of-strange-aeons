50140c6176fad214afca764004d3ac5c20fc7e68
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2021-12-10 08:43:40 +0100

    Gumshoe scrollspy added.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(function () {
 $(document).mouseup(function (e) {
   let container = $("#popupwindow");
 
-  // if the target of the click isn't the container nor a descendant of the container
+  // If the target of the click isn't the container
   if (!container.is(e.target) && container.has(e.target).length === 0) {
     container.hide();
   }
