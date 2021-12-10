d18ee77ccfb83b7cfeb3a996e50e25acba2d68cc
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2021-12-10 07:45:41 +0100

    Toggle sidemenu.

--- main.js
+++ main.js
@@ -13,16 +13,33 @@ $(document).mouseup(function (e) {
   }
 });
 
-/* Sidemenu */
-function menuToggle() {
-  let menuContent = document.getElementById("sidemenu-content");
-  if (menuContent.style.display == "block") {
-    menuContent.style.display = "none";
-    document.getElementById("sidemenu-title").innerHTML =
-      "<h1>⮙ Explore the tales</h1>";
-  } else {
-    menuContent.style.display = "block";
-    document.getElementById("sidemenu-title").innerHTML =
-      "<h1>⮛ Explore the tales</h1>";
+/* Sidemenu with jQuery UI */
+
+$(document).ready(function toggle() {
+  $("#sidemenu-title").click(function () {
+    let title = $("#sidemenu-title");
+
+    $("#sidemenu-content").toggle("slide", { direction: "right" });
+
+    title.toggleClass("expanded");
+
+    if (title.hasClass("expanded")) {
+      title.html("<h1>⮛ Explore the tales</h1>");
+    } else {
+      title.html("<h1>⮙ Explore the tales</h1>");
+    }
+  });
+});
+
+$(document).mouseup(function (e) {
+  let sidemenu = $("#sidemenu");
+  let content = $("#sidemenu-content");
+  let title = $("#sidemenu-title");
+
+  // If the target of the click isn't the container
+  if (!sidemenu.is(e.target) && sidemenu.has(e.target).length === 0) {
+    content.hide("slide", { direction: "right" });
+    title.html("<h1>⮙ Explore the tales</h1>");
+    title.removeClass("expanded");
   }
-}
+});
