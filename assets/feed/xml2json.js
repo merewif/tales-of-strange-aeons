1f6e61d522ec68a7f6b1f1c2da38bead7eb42ca8
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2021-12-14 07:12:31 +0100

    Deleted instructions.

--- assets/feed/xml2json.js
+++ assets/feed/xml2json.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 
 // XML string to be parsed to JSON
 const xml = fs.readFileSync("./assets/feed/feed");
+const url = "https://pasch.substack.com/feed";
 
 // convert XML to JSON
-
-xml2js.parseString(xml, (err, result) => {
+xml2js.parseString(url, (err, result) => {
   if (err) {
     throw err;
   }
