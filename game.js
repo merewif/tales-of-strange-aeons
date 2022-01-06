0208f90ce4a8f85382e26e4179a8780ca1b6710c
Eszter Pálmai <eszterp368@gmail.com>
2022-01-06 14:15:34 +0100

    refactored, etc

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+// global state trackers
+let currentLineIndex = 0;   // Variable to track the current line
+let chapterJson = {};
+
+
 // Function that loads the exposition lines.
 function fetchIntroJson() {
   fetch("./assets/elements/game-assets/intro.json")
@@ -8,10 +13,10 @@ function fetchIntroJson() {
 }
 
 // Function that displays the intro & main menu
-function displayIntro(chapterSentences) {
+function displayIntro(introJson) {
   // Line display tracker
   let i = 0;
-  $("#introtext").text(chapterSentences[0]);
+  $("#introtext").text(introJson[0]);
 
   $(document).on("click keydown", "body", function (e) {
     if (e.keyCode !== 13 && event.type !== "click") {
@@ -22,20 +27,19 @@ function displayIntro(chapterSentences) {
     ++i;
 
     // Loading the main menu after the intro ends
-    if (i >= chapterSentences.length) {
+    if (i >= introJson.length) {
       $("#introtext").css({ display: "none" });
       $("#intro").css({
         "background-image": "url(./assets/covers/book1transparent.png)",
       });
       $("#visualchapternav").css({ display: "block" });
-      chapterSelection();
       return;
     }
 
     // Loading the next line on click with style
     $("#introtext").css({ opacity: "0" });
     setTimeout(function () {
-      $("#introtext").html(chapterSentences[i]);
+      $("#introtext").html(introJson[i]);
     }, 400);
     setTimeout(function () {
       $("#introtext").css({ opacity: "1" });
@@ -43,15 +47,16 @@ function displayIntro(chapterSentences) {
   });
 }
 
-// Function that hides the menu and loads the game interface
-function chapterSelection() {
-  $(document).on("click", ".released", function () {
-    let clickedChapterId = this.id.toString();
-    $("#intro").css({ display: "none" });
-    $("#game").css({ opacity: "1" });
-    fetchChapterJson(clickedChapterId);
-  });
-}
+// clicking on a released chapter's div will trigger fetching it as json
+$(document).on("click", ".released", function () {
+  let clickedChapterId = this.id.toString();
+
+  $("#intro").css({ display: "none" });
+  $("#game").css({ opacity: "1" });
+
+  fetchChapterJson(clickedChapterId);
+});
+
 
 // Function that loads the chapter the user clicked on into the game
 function fetchChapterJson(chapter) {
@@ -59,139 +64,62 @@ function fetchChapterJson(chapter) {
   fetch(urlConstructor)
     .then((response) => response.json())
     .then((data) => {
-      displayChapter(data);
+      chapterJson = data;
+
+      // Loading the initial line of text
+      $("#gametext").text(chapterJson[0]);
     });
 }
 
-// The visual novel function
-function displayChapter(chapterJson) {
-  // Variable to track the current line
-  let currentLine = 0;
 
-  // Loading the initial line of text
-  $("#gametext").text(chapterJson[0]);
+// Click to load the next line of text
+$(document).on("click", "#gametext", function (e) {
+  // Tracker incrementation
+  ++currentLineIndex;
 
-  // Click to load the next line of text
-  $(document).on("click", "#gametext", function (e) {
-    // Halting the narration until the user chooses an option
-    if ($("#gamebutton1").text() !== "") {
-      console.log("Awaiting user response.");
-      return;
+  // Stops trying to load the next line if tracker reaches the length of the array
+  if (currentLineIndex >= chapterJson.length) {
+    return;
+  }
+
+  // Halting the narration until the user chooses an option
+  if ($("#gamebutton1").text() !== "") { // i dont get this condition as a way to check if the user has chosen anything - eszter
+    console.log("Awaiting user response.");
+    return;
+  }
+
+  // If the next item of the chapter list is not a string
+  if (!(typeof chapterJson[currentLineIndex] === "string" || chapterJson[currentLineIndex] instanceof String)) {
+    // display option buttons
+    let optionsObject = Object.values(chapterJson[currentLineIndex]);
+    for (let i = 0; i < optionsObject.length; i++) {
+      $("#gamebutton" + i).text(optionsObject[i][0]);
     }
 
-    // Tracker incrementation
-    ++currentLine;
+    // show the line next after the options in textbox
+    $("#gametext").html(chapterJson[currentLineIndex + 1]);
+  } else {
+    // show line in textbox
+    $("#gametext").html(chapterJson[currentLineIndex]);
+  }
+});
 
-    // Stops trying to load the next line if tracker reaches the length of the array
-    if (currentLine >= chapterJson.length) {
-      return;
-    }
 
-    // If the next item of array is a string it is loaded into the textbox
-    if (
-      typeof chapterJson[currentLine] === "string" ||
-      chapterJson[currentLine] instanceof String
-    ) {
-      $("#gametext").html(chapterJson[currentLine]);
-    } else {
-      // If the next item of array is an object it loads the value text into the buttons
-      let optionsObject = Object.values(chapterJson[currentLine]);
-      for (let i = 0; i < optionsObject.length; i++) {
-        $("#gamebutton" + i).text(optionsObject[i][0]);
-      }
+// Handling user choice
+$(document).on("click", ".gamebutton", function (event) {
+  event.stopPropagation();
 
-      // Handling user choice
+  let optionsObject = Object.values(chapterJson[currentLineIndex]);
+  let buttonNum = $(this).parent().children().index($(this));
+  let linesToSkip = optionsObject[buttonNum][1];
 
-      // Option Button 1
-      $(document).on("click", "#gamebutton0", function (event) {
-        event.stopPropagation();
-        let stepCounter = optionsObject[0][1];
-        console.log(
-          "Before: " +
-            "Current line index: " +
-            currentLine +
-            ", How many lines to skip: " +
-            stepCounter
-        );
+  // Bug happens here
+  currentLineIndex += linesToSkip;
 
-        // Bug happens here
-        $("#gametext").html(chapterJson[currentLine + stepCounter]);
-        currentLine = currentLine + stepCounter;
+  $('.gamebutton').text("");
+  $("#gametext").html(chapterJson[currentLineIndex]);
+});
 
-        console.log(
-          "After: " +
-            "Current line index: " +
-            currentLine +
-            ", How many lines to skip: " +
-            stepCounter
-        );
-
-        // Removing all button text
-        for (let i = 0; i < 3; i++) {
-          $("#gamebutton" + i).text("");
-        }
-      });
-
-      // Option Button 2
-      $("#gamebutton1");
-      $(document).on("click", "#gamebutton1", function (event) {
-        event.stopPropagation();
-        let stepCounter = optionsObject[1][1];
-
-        console.log(
-          "Before: " +
-            "Current line index: " +
-            currentLine +
-            ", How many lines to skip: " +
-            stepCounter
-        );
-
-        // Bug happens here
-        $("#gametext").html(chapterJson[currentLine + stepCounter]);
-        currentLine = currentLine + stepCounter;
-
-        // Get the array index of gametext content and set it as the new index for the text display
-        /* const findTextIndex = (element) =>
-          element === chapterJson[currentLine + stepCounter];
-        currentLine = chapterJson.findIndex(findTextIndex); */
-
-        console.log(
-          "After: " +
-            "Current line index: " +
-            currentLine +
-            ", How many lines to skip: " +
-            stepCounter
-        );
-
-        for (let i = 0; i < 3; i++) {
-          $("#gamebutton" + i).text("");
-        }
-      });
-
-      // Option Button 3
-      $(document).on("click", "#gamebutton2", function (event) {
-        event.stopPropagation();
-        $("#gametext").html(chapterJson[currentLine + optionsObject[2][1]]);
-        currentLine = currentLine + optionsObject[2][1];
-
-        for (let i = 0; i < 3; i++) {
-          $("#gamebutton" + i).text("");
-        }
-      });
-
-      // Option Button 4
-      $(document).on("click", "#gamebutton3", function (event) {
-        event.stopPropagation();
-        $("#gametext").html(chapterJson[currentLine + optionsObject[3][1]]);
-        currentLine = currentLine + optionsObject[3][1];
-
-        for (let i = 0; i < 3; i++) {
-          $("#gamebutton" + i).text("");
-        }
-      });
-    }
-  });
-}
 
 function main() {
   fetchIntroJson();
