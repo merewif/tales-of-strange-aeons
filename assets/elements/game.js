497e99e0c5b809541e7b40d46cc2a572027400cb
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-01-06 01:55:25 +0100

    Visual Novel Intro & Chapter Selection UI added.

--- assets/elements/game.js
+++
@@ -1,3 +0,0 @@
-function main() {}
-
-window.addEventListener("load", main);
---
+++ game.js
@@ -0,0 +1,58 @@
+function fetchIntroJson() {
+  fetch("/assets/elements/game-assets/chapter00.json")
+    .then((response) => response.json())
+    .then((data) => {
+      displayIntro(data);
+    });
+}
+
+function displayIntro(chapterSentences) {
+  let i = 0;
+  $("#introtext").text(chapterSentences[0]);
+
+  $(document).on("click", "body", function (e) {
+    ++i;
+
+    if (i >= chapterSentences.length) {
+      $("#introtext").css({ display: "none" });
+      $("#intro").css({
+        "background-image": "url(./assets/covers/book1.jpg)",
+      });
+      $("#visualchapternav").css({ display: "block" });
+      return;
+    }
+
+    $("#introtext").css({ opacity: "0" });
+
+    setTimeout(function () {
+      $("#introtext").html(chapterSentences[i]);
+    }, 400);
+
+    setTimeout(function () {
+      $("#introtext").css({ opacity: "1" });
+    }, 950);
+  });
+}
+
+function chapterSelection() {
+  $(document).on("click", "#visualchapternav", function () {
+    $("#intro").css({ display: "none" });
+    $("#game").css({ opacity: "1" });
+  });
+}
+
+function fetchChapterJson(chapter) {
+  let urlConstructor = "/assets/elements/game-assets/" + chapter + ".json";
+  fetch(urlConstructor)
+    .then((response) => response.json())
+    .then((data) => {
+      displayChapter(data);
+    });
+}
+
+function main() {
+  fetchIntroJson();
+  chapterSelection();
+}
+
+window.addEventListener("load", main);
