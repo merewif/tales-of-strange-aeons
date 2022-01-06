1d90787e00651cae8c67385466a4827ec947d424
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-01-06 03:18:58 +0100

    First choices added.

--- game.js
+++ game.js
@@ -19,15 +19,14 @@ function displayIntro(chapterSentences) {
         "background-image": "url(./assets/covers/book1transparent.png)",
       });
       $("#visualchapternav").css({ display: "block" });
+      chapterSelection();
       return;
     }
 
     $("#introtext").css({ opacity: "0" });
-
     setTimeout(function () {
       $("#introtext").html(chapterSentences[i]);
     }, 400);
-
     setTimeout(function () {
       $("#introtext").css({ opacity: "1" });
     }, 950);
@@ -38,6 +37,7 @@ function chapterSelection() {
   $(document).on("click", "#visualchapternav", function () {
     $("#intro").css({ display: "none" });
     $("#game").css({ opacity: "1" });
+    fetchChapterJson("chapter01");
   });
 }
 
@@ -50,9 +50,37 @@ function fetchChapterJson(chapter) {
     });
 }
 
+function displayChapter(sentence) {
+  let i = 0;
+  $("#gametext").text(sentence[0]);
+
+  $(document).on("click", "#game", function (e) {
+    ++i;
+
+    if (i >= sentence.length) {
+      return;
+    }
+
+    if (typeof sentence[i] === "string" || sentence[i] instanceof String) {
+      $("#gametext").css({ opacity: "0" });
+      setTimeout(function () {
+        $("#gametext").html(sentence[i]);
+      }, 400);
+      setTimeout(function () {
+        $("#gametext").css({ opacity: "1" });
+      }, 950);
+    } else {
+      const options = Object.values(sentence[i]);
+      console.log(options);
+      for (let i = 0; i < options.length; i++) {
+        $("#gamebutton" + i).text(options[i]);
+      }
+    }
+  });
+}
+
 function main() {
   fetchIntroJson();
-  chapterSelection();
 }
 
 window.addEventListener("load", main);
