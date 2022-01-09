f660256822fa384fd041ed57423537eb2401ba47
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-01-09 22:43:35 +0100

    PWA init.

--- game.js
+++ game.js
@@ -476,4 +476,13 @@ window.addEventListener("load", () => {
     // We've used the prompt, and can't use it again, throw it away
     deferredPrompt = null;
   });
+
+  window.addEventListener("appinstalled", () => {
+    // Hide the app-provided install promotion
+    hideInstallPromotion();
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null;
+    // Optionally, send analytics event to indicate successful install
+    console.log("PWA was installed");
+  });
 });
