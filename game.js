9477cc42d368b10245ee465556db45ff796526a4
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-01-10 01:42:50 +0100

    Do not prevent mini infobar.

--- game.js
+++ game.js
@@ -464,7 +464,7 @@ function installAsPWA() {
 
   window.addEventListener("beforeinstallprompt", (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
+    // e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
