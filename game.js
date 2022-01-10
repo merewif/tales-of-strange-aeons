1e85352b796abe4bebeaa4100bf0134e71982b53
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-01-10 02:41:19 +0100

    Styling and changes.

--- game.js
+++ game.js
@@ -38,6 +38,7 @@ $(document).on("click", "#start", function (e) {
   $("#intro").css({ opacity: "1" });
   $("#start").hide();
   $("#install-game").hide();
+  $("#install-instructions").hide();
   fetchIntroJson();
 
   document.body.requestFullscreen();
@@ -468,8 +469,17 @@ function installAsPWA() {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
+    $("#install-instructions").hide();
     $("#install-game").show();
     showInstallPromotion();
+  });
+
+  $(document).on("click", "#install-instructions", function () {
+    $("#popup").show();
+  });
+
+  $(document).on("click", "#hide-popup, #intro", function () {
+    $("#popup").hide();
   });
 
   const installApp = document.getElementById("install-game");
@@ -482,33 +492,6 @@ function installAsPWA() {
       }
     }
   });
-
-  /*
-  $("#install-game").on("click", function (e) {
-    // hide our user interface that shows our A2HS button
-    $("#install-game").hide();
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  });*/
-  /*
-  window.addEventListener("appinstalled", () => {
-    // Hide the app-provided install promotion
-    hideInstallPromotion();
-    $("#install-game").hide();
-    // Clear the deferredPrompt so it can be garbage collected
-    deferredPrompt = null;
-    // Optionally, send analytics event to indicate successful install
-    console.log("PWA was installed");
-  });*/
 }
 
 window.addEventListener("load", installAsPWA);
